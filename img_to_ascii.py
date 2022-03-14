05fc2b0af41ccceab7030ed2b666c5bb2609216f
Alessandro De Luca <alessandro.deluca@ieu.uzh.ch>
2022-03-14 20:01:56 +0100

    Main function working but neeed to fix PyGame bugs

--- img_to_ascii.py
+++ img_to_ascii.py
@@ -1,41 +1,48 @@
-from genericpath import isfile
 import cv2
-from matplotlib.pyplot import text
+from genericpath import isfile
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
-import io
-
-from pyparsing import col
 
 
 class image:
-    def __init__(self, path, height) -> None:
+    def __init__(self, path, height=48, color_palette=None) -> None:
         """
         Initialize image object.
 
         Args:
-            path: [str]: path to the chosen image
-            height: [int]: image height (after resizing)
+            path: [str]: path to the chosen image.
+            height: [int]: image height after resizing (Default=48).
+            color_palette: [list]: list of colors to use in RGB values tuples (Default=None).
         """
 
-        # load image
+        # input args checking
         if not isfile(path):
             raise FileExistsError('Incorrect path or file does not exist')
 
-        if not type(height) == int:
+        if not type(height) is int:
             raise ValueError(
                 'Expected type int: got type {}'.format(type(height)))
 
+        # load image
         self.img = cv2.imread(path)
 
-        r = self.img.shape[0] / self.img[1]  # image size ratio
+        # grey-scale
+        self.img = np.mean(self.img, -1)
+
+        r = self.img.shape[0] / self.img.shape[1]  # image size ratio
 
         # resize image
         self.img = cv2.resize(self.img, dsize=(height, int(height * r)),
                               interpolation=cv2.INTER_NEAREST)
 
-        # grex-scale
-        self.img = np.mean(self.img, -1)
+        # define color palette
+        if color_palette is None:
+            self.color_palette = [
+                (np.random.randint(0, 255), np.random.randint(
+                    0, 255), np.random.randint(0, 255))
+                for i in range(10)]
+        else:
+            self.color_palette = color_palette
 
     def to_ascii(self) -> None:
         """
@@ -47,10 +54,10 @@ class image:
         l = len(ASCII_CHAR)
 
         self.img_ascii = ''
-        for i in range(0, len(self.img.shape[0])):
-            self.img_ascii += [
+        for i in range(0, self.img.shape[0]):
+            self.img_ascii += "".join([
                 ASCII_CHAR[int((l - 1) * x / 255)] for x in self.img[i]
-            ] + '\n'
+            ]) + '\n'
 
     def start(self, fontname='arial.ttf', fontsize=11, height=200) -> None:
         """
@@ -62,6 +69,8 @@ class image:
             height: [int]: height of final image.
         """
 
+        self.height = height
+
         # define Image Font
         font = ImageFont.truetype(fontname, fontsize)
 
@@ -69,52 +78,58 @@ class image:
         testImg = Image.new('RGB', (1, 1))
         testDraw = ImageDraw.Draw(testImg)
 
-        w, h = testDraw.textsize(self.img_ascii, font)
+        self.w, self.h = testDraw.textsize(self.img_ascii, font)
 
         # create img
-        self.Img = Image.new(mode='RGB', size=(w+4, h+4), color=(0, 0, 0))
+        self.Img = Image.new(mode='RGB', size=(
+            self.w+4, self.h+4), color=(0, 0, 0))
         d = ImageDraw.Draw(self.Img)
         d.text(xy=(0, 0), text=self.img_ascii,
-               fill=(255, 255, 255), align='left')
+               fill=self.color_palette[0], align='left')
 
         # resize
         w, h = self.Img.size
-        r = w / h
-        self.Img = self.Img.resize((int(height*r), height))
+        self.r = w / h
+        self.Img = self.Img.resize((int(self.height*self.r), self.height))
 
         # save image
         self.Img.save('result.png')
 
+        # counter for color palette list
+        self.i = 1
 
-# load image
-path = (input("image path: "))
+    def update(self) -> None:
 
-while not(isfile(path)):
-    print('\033[91m' + 'The file does not exist!' '\033[0m')
-    path = input('Please enter a valid path: ')
+        if self.i == len(self.color_palette):
+            self.i = 0
 
-img = cv2.imread(path)
+        # update image
+        self.Img = Image.new(mode='RGB', size=(
+            self.w+4, self.h+4), color=(0, 0, 0))
+        d = ImageDraw.Draw(self.Img)
+        d.text(xy=(0, 0), text=self.img_ascii,
+               fill=self.color_palette[self.i], align='left')
 
-# resize
-img_ratio = img.shape[0] / img.shape[1]
+        self.Img = self.Img.resize((int(self.height*self.r), self.height))
 
-img = cv2.resize(img, dsize=(48, int(48*img_ratio)),
-                 interpolation=cv2.INTER_NEAREST)
+        self.i += 1
 
-# convert to grey-scale
-img_grey = np.mean(img, -1)
+        # save image
+        self.Img.save('result.png')
 
-# convert to ascii
-img_ascii = np.zeros_like(img_grey, dtype=object)
+        # TODO:
+        #   - PIL.Image directly to PyGame GUI?
 
-for i in range(0, img_ascii.shape[0]):
-    for j in range(0, img_ascii.shape[1]):
-        img_ascii[i, j] = to_ascii(img_grey[i, j])
 
-img_ascii2 = np.zeros_like(img_grey, dtype=np.dtype('U1'))
+if __name__ == '__main__':
+    path = 'test_images/adidas.jpg'
 
-for i in range(0, img_ascii2.shape[0]):
-    img_ascii2[i] = [
-        ASCII_CHAR[int((len(ASCII_CHAR)-1) * x / 255.0)] for x in img_grey[i]]
+    img = image(path=path)
 
-print(np.all(img_ascii == img_ascii2))
+    img.to_ascii()
+
+    img.start()
+
+    pause = input('Press <ENTER> to continue...')
+
+    img.update()
--- screensaver.py
+++ screensaver.py
@@ -1,46 +1,216 @@
-import pygame
+import ctypes
 import time
-from numpy.random import uniform
+import tkinter as tk
 
-pygame.init()
+import cv2
+from genericpath import isfile
+import numpy as np
+from PIL import Image, ImageDraw, ImageFont
+import pygame
 
-# params
-screenWidth, screenHeight = 1920, 1080
-bgColor = 0, 0, 0
-speed = [uniform(1.75, 2.25), uniform(1.75, 2.25)]
 
-# initialize screen
-screen = pygame.display.set_mode((screenWidth, screenHeight))
+class image:
+    def __init__(self, path, height=48, color_palette=None) -> None:
+        """
+        Initialize image object.
 
-# load image
-img = pygame.image.load('result.jpg')
+        Args:
+            path: [str]: path to the chosen image.
+            height: [int]: image height after resizing (Default=48).
+            color_palette: [list]: list of colors to use in RGB values tuples (Default=None).
+        """
 
-# resize
-# img = pygame.transform.scale(img, (100, 100))
+        # input args checking
+        if not isfile(path):
+            raise FileExistsError('Incorrect path or file does not exist')
 
-# create rectangle
-imgRect = img.get_rect()
+        if not type(height) is int:
+            raise ValueError(
+                'Expected type int: got type {}'.format(type(height)))
 
-# animate
-while True:
+        # load image
+        self.img = cv2.imread(path)
 
-    # background color fill
-    screen.fill(color=bgColor)
+        # grey-scale
+        self.img = np.mean(self.img, -1)
 
-    # add img
-    screen.blit(img, imgRect)
+        r = self.img.shape[0] / self.img.shape[1]  # image size ratio
 
-    # moving
-    imgRect = imgRect.move(speed)
+        # resize image
+        self.img = cv2.resize(self.img, dsize=(height, int(height * r)),
+                              interpolation=cv2.INTER_NEAREST)
 
-    # collision
-    if imgRect.left < 0 or imgRect.right > screenWidth:
-        speed[0] *= -1
-        img = pygame.image.load('result2.jpg')  # mock update
+        # define color palette
+        if color_palette is None:
+            self.color_palette = [
+                (np.random.randint(0, 255), np.random.randint(
+                    0, 255), np.random.randint(0, 255))
+                for i in range(10)]
+        else:
+            self.color_palette = color_palette
 
-    if imgRect.top < 0 or imgRect.bottom > screenHeight:
-        speed[1] *= -1
+    def to_ascii(self) -> None:
+        """
+        Converts image to ascii art.
+        """
 
-    # refresh screen
-    pygame.display.flip()
-    time.sleep(10/1000)
+        # characters to use
+        ASCII_CHAR = "B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+        l = len(ASCII_CHAR)
+
+        self.img_ascii = ''
+        for i in range(0, self.img.shape[0]):
+            self.img_ascii += "".join([
+                ASCII_CHAR[int((l - 1) * x / 255)] for x in self.img[i]
+            ]) + '\n'
+
+    def start(self, fontname='arial.ttf', fontsize=11, height=200):
+        """
+        Create first image.
+
+        Args:
+            fontname: [str]: font to use for the ascii art (Default: Arial).
+            fontsize: [int]: font size.
+            height: [int]: height of final image.
+
+        Returns:
+            PyGame image to animate.
+        """
+
+        self.height = height
+
+        # define Image Font
+        font = ImageFont.truetype(fontname, fontsize)
+
+        # get measurements
+        testImg = Image.new('RGB', (1, 1))
+        testDraw = ImageDraw.Draw(testImg)
+
+        self.w, self.h = testDraw.textsize(self.img_ascii, font)
+
+        # create img
+        self.Img = Image.new(mode='RGB', size=(
+            self.w+4, self.h+4), color=(0, 0, 0))
+        d = ImageDraw.Draw(self.Img)
+        d.text(xy=(0, 0), text=self.img_ascii,
+               fill=self.color_palette[0], align='left')
+
+        # resize
+        w, h = self.Img.size
+        self.r = w / h
+        self.Img = self.Img.resize((int(self.height*self.r), self.height))
+
+        # save image
+        self.Img.save('result.png')
+
+        # counter for color palette list
+        self.i = 1
+
+        # load in pygame
+        return pygame.image.load('result.png')
+
+    def update(self):
+        """
+        Update image.
+
+        Returns:
+            PyGame image.
+        """
+
+        if self.i == len(self.color_palette):
+            self.i = 0
+
+        # update image
+        self.Img = Image.new(mode='RGB', size=(
+            self.w+4, self.h+4), color=(0, 0, 0))
+        d = ImageDraw.Draw(self.Img)
+        d.text(xy=(0, 0), text=self.img_ascii,
+               fill=self.color_palette[self.i], align='left')
+
+        self.Img = self.Img.resize((int(self.height*self.r), self.height))
+
+        self.i += 1
+
+        # save image
+        self.Img.save('result.png')
+
+        return pygame.image.load('result.png')
+
+
+def get_display_size():
+    try:  # Windows 8.1 and later
+        ctypes.windll.shcore.SetProcessDpiAwareness(2)
+    except Exception as e:
+        pass
+    try:  # Before Windows 8.1
+        ctypes.windll.user32.SetProcessDPIAware()
+    except:  # Windows 8 or before
+        pass
+
+    root = tk.Tk()
+    root.update_idletasks()
+    root.attributes('-fullscreen', True)
+    root.state('iconic')
+    height = root.winfo_screenheight()
+    width = root.winfo_screenwidth()
+    root.destroy()
+
+    return height, width
+
+
+def main(img_path):
+
+    # image class
+    Img = image(
+        path=img_path
+    )
+    Img.to_ascii()
+    img = Img.start()
+
+    # initialize pygame
+    pygame.init()
+
+    # params
+    screenHeight, screenWidth = get_display_size()
+    bgColor = (0, 0, 0)
+    speed = [
+        np.random.uniform(1.75, 2.25),
+        np.random.uniform(1.75, 2.25)
+    ]
+
+    # initialize screen
+    screen = pygame.display.set_mode((screenWidth, screenHeight))
+
+    # create rectangle
+    imgRect = img.get_rect()
+
+    # animate
+    while True:
+
+        # background color fill
+        screen.fill(color=bgColor)
+
+        # add img
+        screen.blit(img, imgRect)
+
+        # moving
+        imgRect = imgRect.move(speed)
+
+        # collision
+        if imgRect.left < 0 or imgRect.right > screenWidth:
+            speed[0] *= -1
+            img = Img.update()
+
+        if imgRect.top < 0 or imgRect.bottom > screenHeight:
+            speed[1] *= -1
+            img = Img.update()
+
+        # refresh screen
+        pygame.display.flip()
+        time.sleep(10/1000)
+
+
+if __name__ == '__main__':
+    main(img_path='test_images/adidas.jpg')
+
+# TODO: Pygame window not focusing at beginning and freezing on any action
